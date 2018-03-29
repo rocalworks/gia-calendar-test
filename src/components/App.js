bbb62e5340101739ca67470506744ee7dff84e71
Krisha Orge (Ryxie) <rocalworks@gmail.com>
2018-03-29 21:57:34 +0800

    Add validation on date inputs

--- src/components/App.js
+++ src/components/App.js
@@ -27,7 +27,8 @@ export default class App extends Component{
             startDate: null,
             endDate: null,
             focusedInput: null,
-            blockedDates: []
+            blockedDates: [],
+            bookingNotif: "Add some dates here."
         };
     }
 
@@ -93,14 +94,25 @@ export default class App extends Component{
         e.preventDefault();
 
         let formData = new FormData(this.bookingForm);
+        let notif = this.validateDate(formData);
 
-        // Verify if date is in blocked dates
-        if ( this.isDateBlocked(formData) ) {
-            // Throw error if
-            console.log("Someone booked this already!");
+        // Verify if date is not empty
+        if (notif !== "OK") {
+            this.setState( () => {
+                return {
+                    bookingNotif: notif
+                }
+            });
             return;
+        } else {
+            this.setState( () => {
+                return {
+                    bookingNotif: "Dates are valid. Submitting to server..."
+                }
+            });
         }
 
+        // Send data to the API server
         let params = {
             method: 'post',
             body: formData
@@ -108,34 +120,57 @@ export default class App extends Component{
 
         fetch(server_url + '/create-event/', params)
             .then( response => { return response.text() })
-            .then(  data => { console.log(data) });
-
-        this.getCalendar();
+            .then(  data => {
+                this.setState( () => {
+                    return {
+                        blockedDates: [],
+                        bookingNotif: "Booked successfully. Refreshing calendar..."
+                    }
+                });
+                // Refresh page
+                // TODO: Refresh calendar after 3 seconds
+                setTimeout(function(){ window.location.reload() }, 3000);
+            });
     }
 
-    isDateBlocked(form) {
+    validateDate(form) {
         // Extract FormData entries
         let startDate = "";
         let endDate = "";
 
         for (var entry of form.entries()) {
-            if (entry[0] === "start_date") { startDate = moment(entry[1]); }
-            else if (entry[0] === "end_date") {endDate = moment(entry[1]); }
+            if (entry[0] === "start_date") { startDate = entry[1]; }
+            else if (entry[0] === "end_date") {endDate = entry[1]; }
+        }
+
+        // VERIFY: Check if dates are empty
+        if (startDate === "" || endDate === "") {
+            return "ERROR: Dates are empty!";
+        }
+
+        // Convert dates into moments object
+        let start = moment(startDate);
+        let end = moment(endDate);
+
+        // VERIFY: Check for reverse date range
+        if (start > end) {
+            return "ERROR: End date is greater than start date!";
         }
 
         // Get range
-        let range = getRange(startDate, endDate);
+        let range = getRange(start, end);
 
         // Check if date is within blocked dates
         for(let i = 0; i < range.length; i++) {
             for(let j = 0; j < this.state.blockedDates.length; j++) {
                 if (range[i].format('YYYY-MM-DD') === this.state.blockedDates[j].format('YYYY-MM-DD')) {
-                    return true;
+                    return "ERROR: Someone is already booked on this day!";
                 }
             }
         }
 
-        return false;
+        // Input is fine; ready for sending data
+        return "OK";
     }
 
     render() {
@@ -154,10 +189,14 @@ export default class App extends Component{
         return(
             <div className="container">
                 <form ref={(form) => { this.bookingForm = form } }>
+                    <p style={{marginTop: 25 + "px"}}>{this.state.bookingNotif}</p>
+                    <label style={{margin: "0 25px 0 25px"}}>Start date:</label>
                     <input type="date" name="start_date" onChange={({ startDate }) => this.setState({ startDate })} />
+                    <label style={{margin: "0 25px 0 25px"}}>End date:</label>
                     <input type="date" name="end_date" onChange={({ endDate }) => this.setState({ endDate })} />
-                    <Button bsSize="large" onClick={this.createEvent.bind(this)}>Submit</Button>
+                    <Button bsSize="large" onClick={this.createEvent.bind(this)} style={{margin: "0 25px 0 25px"}}>Submit</Button>
                 </form>
+                <hr />
                 {calendarContainer}
             </div>
         );
